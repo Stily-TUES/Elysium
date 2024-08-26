434aadf73675ec5d97d79c50738d6e25250c1a18
Stily-TUES <stilianqo@gmail.com>
2024-08-26 23:10:06 +0300

    project templates and some utility classes

---
+++ Editor/GameProject/NewProject.cs
@@ -0,0 +1,98 @@
+﻿using Editor.GameProject.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Xps;
+
+namespace Editor.GameProject
+{
+    //needed for deserialization
+    [DataContract]
+    public class ProjectTemplate
+    {
+        //we use DataMemeber to serialize the fields
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+        public string IconPath { get; set; }
+        public string ProjectPath { get; set; }
+        public string ScreenshotPath { get; set; }
+    }
+    class NewProject : BaseViewModel
+    {
+        //TODO: get the path from the instalation location
+        private readonly string _templatePath = @"..\..\..\..\Editor\Templates";
+        private string _projectName = "New Project";
+        public string ProjectName 
+        { 
+            get => _projectName;
+            set 
+            {  
+                if(_projectName != value)
+                {
+                    _projectName = value;
+                    OnPropertyChanged(nameof(ProjectName));
+                }
+            } 
+        }
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\Elysium\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    OnPropertyChanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        //we dont want to allow the user to change the templates so we use readonly
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates { get; }
+         
+        public NewProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconPath);
+                    template.ScreenshotPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotPath);
+                    template.ProjectPath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+
+                    _projectTemplates.Add(template);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+            }
+            //TODO: log error
+        }
+
+    }
+
+
+}
---
+++ Editor/GameProject/Utils/Serializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace Editor.GameProject.Utils
+{
+    public static class Serializer
+    {
+        public static void ToFile<T>(T instance, string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Create);
+                var serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(fs, instance);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                //TODO: Log error
+            }
+        }
+        public static T FromFile<T>(string path)
+        {
+            try
+            {
+                using var fs = new FileStream(path, FileMode.Open);
+                var serializer = new DataContractSerializer(typeof(T));
+                T instance = (T)serializer.ReadObject(fs );
+                return instance;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return default(T);
+                //TODO: Log error
+            }
+        }
+    }
+}
