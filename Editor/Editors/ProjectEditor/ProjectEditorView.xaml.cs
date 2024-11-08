60a1cca76a91f698119ecbd6dd1ef86991021372
Stily <stilianqo@gmail.com>
2024-11-08 16:40:29 +0200

    feat: embed OpenTK window in wpf and rendering game entities

--- Editor/Editors/ProjectEditor/ProjectEditorView.xaml.cs
+++ Editor/Editors/ProjectEditor/ProjectEditorView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Editor.GameProject;
 using Editor.Utils;
+using OpenTK.GLControl;
+using OpenTK.Graphics.OpenGL4;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,27 +24,59 @@ namespace Editor.Editors;
 /// </summary>
 public partial class ProjectEditorView : UserControl
 {
+    private GLControl glControl;
+    private ProjectManager projectManager;
+
     public ProjectEditorView()
     {
         InitializeComponent();
+        Loaded += ProjectEditorView_Loaded;
+    }
+
+    private void ProjectEditorView_Loaded(object sender, RoutedEventArgs e)
+    {
+        glControl = new GLControl();
+        glControl.Paint += GlControl_Paint;
+        glControl.Resize += GlControl_Resize;
+        windowsFormsHost.Child = glControl;
+
+        var window = Window.GetWindow(this);
+        projectManager = (ProjectManager)window.DataContext;
+    }
+
+
+    private void GlControl_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+    {
+        Render();
+    }
+
+    private void GlControl_Resize(object sender, EventArgs e)
+    {
+        GL.Viewport(0, 0, glControl.Width, glControl.Height);
+    }
+
+    private void Render()
+    {
+        if (projectManager != null)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            projectManager.RenderProject();
+            glControl.SwapBuffers();
+        }
     }
 
     private void onUndoButton_Click(object sender, RoutedEventArgs e)
     {
-        var window = Window.GetWindow(this);
-        var projectManager = (ProjectManager)window.DataContext;
         projectManager?.Undo();
     }
+
     private void onRedoButton_Click(object sender, RoutedEventArgs e)
     {
-        var window = Window.GetWindow(this);
-        var projectManager = (ProjectManager)window.DataContext;
         projectManager?.Redo();
     }
+
     private void onSaveButton_Click(object sender, RoutedEventArgs e)
     {
-        var window = Window.GetWindow(this);
-        var projectManager = (ProjectManager)window.DataContext;
         projectManager?.Save();
     }
 }
